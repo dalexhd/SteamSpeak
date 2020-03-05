5c78a69549da6b4be1db118bcabd0f79ce909870
DalexHD <eldalexhd@gmail.com>
2020-03-05 20:15:09 +0100

    Added frontend.

---
+++ src/backend/config/redis.js
@@ -0,0 +1,6 @@
+const redis = {
+  ip: '127.0.0.1',
+  port: '6379',
+};
+
+module.exports = redis;
---
+++ src/backend/config/teamspeak.js
@@ -0,0 +1,12 @@
+const teamspeak = {
+  ip: '127.0.0.1',
+  query_port: 10011,
+  port: 9987,
+  login: 'serveradmin',
+  password: '',
+  name: 'SteamSpeak',
+  server_id: 1,
+  default_channel: 1,
+};
+
+module.exports = teamspeak;
