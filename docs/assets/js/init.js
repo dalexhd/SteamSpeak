14c2073b28e9d3d9a8a6c923f370bb3738cf660f
DalexHD <eldalexhd@gmail.com>
2020-03-05 16:14:13 +0100

    Added editorconfig.

--- docs/assets/js/init.js
+++ docs/assets/js/init.js
@@ -1,52 +1,52 @@
 function setEnvioroment() {
-	const domAnimator = new DomAnimator();
-	console.log(
-	  `%c Docsify %c ${window.Docsify.version} %c  %c Detected environment %c Production %c  %c Panel by: %c Docsify %c`,
-	  "background:#35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
-	  "background:#1972F5; padding: 1px; border-radius: 0 3px 3px 0; color: #fff;",
-	  "background:transparent",
-	  "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
-	  "background: #1972F5; padding: 1px; border-radius: 0 3px 3px 0; color: #fff;",
-	  "background:transparent",
-	  "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
-	  "background: #1972F5; padding: 1px; border-radius: 0 3px 3px 0; color: #fff;",
-	  "background:transparent"
-	);
-	const frame1 = [
-	`       .-"-.       𝘿𝙚𝙩𝙚𝙘𝙩𝙚𝙙 𝙚𝙣𝙫𝙞𝙧𝙤𝙣𝙢𝙚𝙣𝙩: Production`,
-	`     _/.-.-.\\_     𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${window.Docsify.version}`,
-	"    ( ( o o ) )    𝙋𝙖𝙣𝙚𝙡 𝘽𝙮: Docsify",
-	'     |/  "  \\|     ',
-	"      \\'/^\\'/      ",
-	"      /`\\ /`\\      ",
-	"     /  /|\\  \\     ",
-	"    ( (/ T \\) )    ",
-	"     \\__/^\\__/     "
-	];
-	const frame2 = [
-	`       .-"-.       𝘿𝙚𝙩𝙚𝙘𝙩𝙚𝙙 𝙚𝙣𝙫𝙞𝙧𝙤𝙣𝙢𝙚𝙣𝙩: Production`,
-	`     _/_-.-_\\_     𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${window.Docsify.version}`,
-	"    / __> <__ \\    𝙋𝙖𝙣𝙚𝙡 𝘽𝙮: Docsify",
-	'   / //  "  \\\\ \\   ',
-	"  / / \\'---'/ \\ \\  ",
-	'  \\ \\_/`"""`\\_/ /  ',
-	"   \\           /   ",
-	"    \\         /    ",
-	"     |   .   |     "
-	];
-	const frame3 = [
-	`       .-"-.       𝘿𝙚𝙩𝙚𝙘𝙩𝙚𝙙 𝙚𝙣𝙫𝙞𝙧𝙤𝙣𝙢𝙚𝙣𝙩: Production`,
-	`     _/_-.-_\\_     𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${window.Docsify.version}`,
-	"    /|( o o )|\\    𝙋𝙖𝙣𝙚𝙡 𝘽𝙮: Docsify",
-	'   | //  "  \\\\ |   ',
-	"  / / \\'---'/ \\ \\  ",
-	'  \\ \\_/`"""`\\_/ /  ',
-	"   \\           /   ",
-	"    \\         /    ",
-	"     |   .   |     "
-	];
-	domAnimator.addFrame(frame1);
-	domAnimator.addFrame(frame2);
-	domAnimator.addFrame(frame3);
-	domAnimator.animate(1000);
+  const domAnimator = new DomAnimator();
+  console.log(
+    `%c Docsify %c ${window.Docsify.version} %c  %c Detected environment %c Production %c  %c Panel by: %c Docsify %c`,
+    "background:#35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
+    "background:#1972F5; padding: 1px; border-radius: 0 3px 3px 0; color: #fff;",
+    "background:transparent",
+    "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
+    "background: #1972F5; padding: 1px; border-radius: 0 3px 3px 0; color: #fff;",
+    "background:transparent",
+    "background: #35495e; padding: 1px; border-radius: 3px 0 0 3px; color: #fff;",
+    "background: #1972F5; padding: 1px; border-radius: 0 3px 3px 0; color: #fff;",
+    "background:transparent"
+  );
+  const frame1 = [
+    `       .-"-.       𝘿𝙚𝙩𝙚𝙘𝙩𝙚𝙙 𝙚𝙣𝙫𝙞𝙧𝙤𝙣𝙢𝙚𝙣𝙩: Production`,
+    `     _/.-.-.\\_     𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${window.Docsify.version}`,
+    "    ( ( o o ) )    𝙋𝙖𝙣𝙚𝙡 𝘽𝙮: Docsify",
+    '     |/  "  \\|     ',
+    "      \\'/^\\'/      ",
+    "      /`\\ /`\\      ",
+    "     /  /|\\  \\     ",
+    "    ( (/ T \\) )    ",
+    "     \\__/^\\__/     "
+  ];
+  const frame2 = [
+    `       .-"-.       𝘿𝙚𝙩𝙚𝙘𝙩𝙚𝙙 𝙚𝙣𝙫𝙞𝙧𝙤𝙣𝙢𝙚𝙣𝙩: Production`,
+    `     _/_-.-_\\_     𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${window.Docsify.version}`,
+    "    / __> <__ \\    𝙋𝙖𝙣𝙚𝙡 𝘽𝙮: Docsify",
+    '   / //  "  \\\\ \\   ',
+    "  / / \\'---'/ \\ \\  ",
+    '  \\ \\_/`"""`\\_/ /  ',
+    "   \\           /   ",
+    "    \\         /    ",
+    "     |   .   |     "
+  ];
+  const frame3 = [
+    `       .-"-.       𝘿𝙚𝙩𝙚𝙘𝙩𝙚𝙙 𝙚𝙣𝙫𝙞𝙧𝙤𝙣𝙢𝙚𝙣𝙩: Production`,
+    `     _/_-.-_\\_     𝙑𝙚𝙧𝙨𝙞𝙤𝙣: ${window.Docsify.version}`,
+    "    /|( o o )|\\    𝙋𝙖𝙣𝙚𝙡 𝘽𝙮: Docsify",
+    '   | //  "  \\\\ |   ',
+    "  / / \\'---'/ \\ \\  ",
+    '  \\ \\_/`"""`\\_/ /  ',
+    "   \\           /   ",
+    "    \\         /    ",
+    "     |   .   |     "
+  ];
+  domAnimator.addFrame(frame1);
+  domAnimator.addFrame(frame2);
+  domAnimator.addFrame(frame3);
+  domAnimator.animate(1000);
 }
--- docs/assets/js/libs/domAnimator.js
+++ docs/assets/js/libs/domAnimator.js
@@ -9,161 +9,161 @@
  * DomAnimator
  *********************************************/
 
- var DomAnimator = (function() {
+var DomAnimator = (function () {
 
-    'use strict';
+  'use strict';
 
-    var currentFrame = 0;
-    var frames = [];
-    var nodes = [];
+  var currentFrame = 0;
+  var frames = [];
+  var nodes = [];
 
-    // Chrome console shows new lines, others don't...
-    // so multiple comments are used on others, to look good.
-    var multiNode = !!!window.chrome;
+  // Chrome console shows new lines, others don't...
+  // so multiple comments are used on others, to look good.
+  var multiNode = !!!window.chrome;
 
-    var interval = null;
-    var defaultTime = 500; // ms
-    var attached = false;
-    var whiteSpaceString = '\u00A0';
+  var interval = null;
+  var defaultTime = 500; // ms
+  var attached = false;
+  var whiteSpaceString = '\u00A0';
 
-    // Soft object augmentation
-    function extend( target, source ) {
-        for ( var key in source ) {
-            if ( !( key in target ) ) {
-                target[ key ] = source[ key ];
-            }
-        }
-        return target;
+  // Soft object augmentation
+  function extend(target, source) {
+    for (var key in source) {
+      if (!(key in target)) {
+        target[key] = source[key];
+      }
+    }
+    return target;
+  }
+
+  // Frame passed through as a list []
+  function parseMultilineFrame(frame) {
+    if (multiNode) {
+      return swapWhitespace(frame);
+    } else {
+      return padString(frame.join('\n'));
+    }
+  }
+
+  // Frame passed through as a string.
+  function parseSingleLineFrame(frame) {
+    if (multiNode) {
+      return swapWhitespace(frame.split('\n'));
+    } else {
+      return padString(frame);
+    }
+  }
+
+  function swapWhitespace(array) {
+    var i = 0;
+
+    for (i; i < array.length; i++) {
+      array[i] = array[i].replace(/ /g, whiteSpaceString);
+    }
+    return array;
+  }
+
+  function animate(time) {
+
+    // No time set, just use default!
+    if (!time) {
+      time = defaultTime;
     }
 
-    // Frame passed through as a list []
-    function parseMultilineFrame( frame ) {
-        if(multiNode) {
-            return swapWhitespace(frame);
-        } else {
-            return padString(frame.join('\n'));
-        }
+    // No frames
+    if (frames.length === 0) {
+      console.log("I need frames to animate. You can add them with .addFrame( string )");
+      return;
     }
 
-    // Frame passed through as a string.
-    function parseSingleLineFrame( frame ) {
-        if (multiNode) {
-            return swapWhitespace(frame.split('\n'));
-        } else {
-            return padString(frame);
-        }
+    if (attached === false) {
+      attachToDocument();
     }
 
-    function swapWhitespace( array ) {
-        var i = 0;
+    interval = setInterval(function () {
+      renderFrame();
+    }, time);
+  }
 
-        for( i; i < array.length; i++ ) {
-            array[i] = array[i].replace(/ /g, whiteSpaceString);
-        }
-        return array;
+  function renderFrame() {
+
+    var frameData = frames[currentFrame];
+
+    if (multiNode) {
+
+      var i = 0;
+      for (i; i < nodes.length; i++) {
+        nodes[i].nodeValue = frameData[i];
+      }
+
+    } else {
+      nodes[0].nodeValue = frameData;
     }
 
-    function animate( time ) {
+    currentFrame = currentFrame + 1;
+    if (currentFrame === frames.length) {
+      currentFrame = 0;
+    }
+  }
 
-        // No time set, just use default!
-        if( !time ) {
-            time = defaultTime;
-        }
+  function attachToDocument() {
 
-        // No frames
-        if( frames.length === 0) {
-            console.log( "I need frames to animate. You can add them with .addFrame( string )" );
-            return;
-        }
+    var head = document.head;
+    var parent = head.parentNode;
 
-        if( attached === false ) {
-            attachToDocument();
-        }
+    // This assumes you have the same amount of frames in each section.
+    if (multiNode) {
 
-        interval = setInterval( function() {
-            renderFrame();
-        }, time);
+      var i = 0;
+      var totalNodes = frames[0].length;
+      for (i; i < totalNodes; i++) {
+        var node = document.createComment('');
+        nodes.push(node);
+        parent.insertBefore(node, head);
+      }
+
+    } else {
+      var node = document.createComment('');
+      nodes.push(node);
+      parent.insertBefore(node, head);
+    }
+  }
+
+  function stop() {
+    clearInterval(interval);
+  }
+
+  function addFrame(frameData) {
+
+    if (!frameData) {
+      frameData = 'no frame data';
     }
 
-    function renderFrame() {
+    var frameType = typeof (frameData);
 
-        var frameData = frames[currentFrame];
+    // Multi line data.
+    if (frameType === 'object') {
 
-        if( multiNode ) {
+      frames.push(parseMultilineFrame(frameData));
 
-            var i = 0;
-            for( i; i < nodes.length; i++ ) {
-                nodes[i].nodeValue = frameData[i];
-            }
+      // String data
+    } else if (frameType === 'string') {
 
-        } else {
-            nodes[0].nodeValue = frameData;
-        }
-
-        currentFrame = currentFrame + 1;
-        if ( currentFrame === frames.length ) {
-            currentFrame = 0;
-        }
+      frames.push(parseSingleLineFrame(frameData));
     }
+  }
 
-    function attachToDocument() {
+  function padString(string) {
+    return "\n" + string + '\n';
+  }
 
-        var head = document.head;
-        var parent = head.parentNode;
+  function main() {
+  }
 
-        // This assumes you have the same amount of frames in each section.
-        if ( multiNode ) {
-
-            var i = 0;
-            var totalNodes = frames[0].length;
-            for( i; i < totalNodes; i++ ) {
-                var node = document.createComment('');
-                nodes.push(node);
-                parent.insertBefore( node, head);
-            }
-
-        } else {
-            var node = document.createComment('');
-            nodes.push(node);
-            parent.insertBefore(node, head);
-        }
-    }
-
-    function stop() {
-        clearInterval(interval);
-    }
-
-    function addFrame( frameData ) {
-
-        if( !frameData ) {
-            frameData = 'no frame data';
-        }
-
-        var frameType = typeof(frameData);
-
-        // Multi line data.
-        if( frameType === 'object' ) {
-
-            frames.push( parseMultilineFrame(frameData) );
-
-        // String data
-        } else if ( frameType === 'string' ) {
-
-            frames.push( parseSingleLineFrame(frameData) );
-        }
-    }
-
-    function padString(string) {
-        return "\n" + string + '\n';
-    }
-
-    function main() {
-    }
-
-    return extend( main, {
-        addFrame: addFrame,
-        animate: animate,
-        stop: stop
-    });
+  return extend(main, {
+    addFrame: addFrame,
+    animate: animate,
+    stop: stop
+  });
 
 });
