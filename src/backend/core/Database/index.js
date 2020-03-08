9bc5a473fdf38b038cc42280de151b3884635ebd
DalexHD <eldalexhd@gmail.com>
2020-03-08 13:02:10 +0100

    Added Mysql connection class.

---
+++ src/backend/core/Database/index.js
@@ -0,0 +1,35 @@
+const {
+  EventEmitter,
+} = require('events');
+const mysql = require('mysql');
+const log = require('../../utils/log.js');
+const config = require('../../config/database.js');
+
+/* !fix Problema al renombrar los nombres...
+Parece que los nombres o algo no cuadrán con la cache que esta bien.
+*/
+class Database extends EventEmitter {
+  /**
+   * Construct the Database class.
+   */
+  constructor() {
+    super();
+    try {
+      this.database = mysql.createConnection({
+        host: config.ip,
+        user: config.user,
+        password: config.database.password,
+        database: config.database,
+        debug: config.debug,
+      });
+    } catch (error) {
+      this.emit('connection_error', error);
+    }
+    setInterval(() => {
+      this.database.query('SELECT 1');
+      log.debug('Sent query to prevent disconnect ^^');
+    }, 600000);
+  }
+}
+
+module.exports = Database;
