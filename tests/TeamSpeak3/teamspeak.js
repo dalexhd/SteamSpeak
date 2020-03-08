4bd384c0aa8d09866eff15de6c5c1181c67315c3
DalexHD <eldalexhd@gmail.com>
2020-03-08 13:27:16 +0100

    Fixed tests. (100% sure :-P)

--- tests/TeamSpeak3/teamspeak.js
+++ tests/TeamSpeak3/teamspeak.js
@@ -1,8 +1,7 @@
 /* global describe beforeEach it */
-jest.setTimeout(30000);
 
 const { TeamSpeak } = require('ts3-nodejs-library');
-const config = require('../../src/backend/config/teamspeak');
+const config = require('../../src/backend/config/teamspeak').teamspeak;
 
 const ts3 = new TeamSpeak({
   protocol: 'ssh',
@@ -21,27 +20,23 @@ describe('TeamSpeak3', () => {
     await ts3.quit();
   });
   describe('Conection', () => {
-    it('Check if we have connection...', async (done) => {
+    it('Check if we have connection...', async () => {
       const data = await ts3.whoami();
       expect(data).not.toBe(null);
-      done();
     });
-    it('should verify parameters of #hostInfo()', async (done) => {
+    it('should verify parameters of #hostInfo()', async () => {
       const data = await ts3.hostInfo();
       expect(data).not.toBe(null);
-      done();
     });
   });
   describe('Commands', () => {
-    it('should verify parameters of #serverGroupClientList()', async (done) => {
+    it('should verify parameters of #serverGroupClientList()', async () => {
       const data = ts3.serverGroupClientList(1);
       expect(data).not.toBe(null);
-      done();
     });
-    it('should verify parameters of #sendTextMessage()', async (done) => {
+    it('should verify parameters of #sendTextMessage()', async () => {
       const data = await ts3.sendTextMessage(10, 2, 'Test message');
       expect(data).not.toBe(null);
-      done();
     });
   });
 });
