1205f60987f923bf4bff08d5eee176056c723b7b
Alex <thedalexhd@gmail.com>
2020-03-20 02:42:23 +0100

    Added full pwa vue environment with firebase.

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -1,12 +1,33 @@
 import Vue from 'vue';
+import Toasted from 'vue-toasted';
+import Push from 'push.js';
 import App from './App.vue';
+
+import './lib/SteamSpeakCss';
+
 import router from './router';
 import store from './store';
+import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+Vue.use(Toasted);
 
-new Vue({
+if (!Push.Permission.has()) {
+  Push.Permission.request(() => {
+    Push.create('SteamSpeak!', {
+      body: 'Web notifications enabled!',
+      icon: '/img/icons/apple-touch-icon-76x76.png',
+      timeout: 5000,
+      onClick() {
+        window.focus();
+        this.close();
+      },
+    });
+  });
+}
+
+window.App = new Vue({
   router,
   store,
-  render: h => h(App),
+  render: (h) => h(App),
 }).$mount('#app');
---
+++ src/frontend/vue.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  chainWebpack: (config) => {
+    config
+      .plugin('html')
+      .tap((args) => {
+        // eslint-disable-next-line no-param-reassign
+        args[0].title = 'SteamSpeak';
+        return args;
+      });
+  },
+  pwa: {
+    name: 'SteamSpeak',
+    themeColor: '#000000',
+    msTileColor: '#da532c',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+    gcm_sender_id: '1021489007211',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'src/service-worker.js',
+    },
+  },
+  integrity: true,
+};
