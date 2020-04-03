0cb768c35c895d8be6afdb1aa56fdaa31ab7d247
DalexHD <eldalexhd@gmail.com>
2020-04-03 21:21:02 +0200

    Removed unecesary files.

--- src/frontend/src/main.js
+++ src/frontend/src/main.js
@@ -95,9 +95,6 @@ require('./assets/css/iconfont.css');
 // Flags icons
 require('./assets/css/flags.css');
 
-// Loader
-require('./assets/css/loader.css');
-
 // Vue select css
 // Note: In latest version you have to add it separately
 // import 'vue-select/dist/vue-select.css';
