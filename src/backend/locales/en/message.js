ce3ce180ac81084bcd8cbe382ebc0ab5f023201d
DalexHD <eldalexhd@gmail.com>
2020-04-05 19:12:21 +0200

    Add backend locale files.

---
+++ src/backend/locales/en/message.js
@@ -0,0 +1,4 @@
+module.exports = {
+	login_msg:
+		"Hello {NICKNAME} :-) \n You've recieved a login request from SteamSpeak...\n[COLOR=#ff5500]Follow this steps[/COLOR]\n [u]If you made this request:[/u]\n - Copy and paste the following code: [color=orange][b]{TOKEN}[/b][/color]\n' [u]You didn't make this request:[/u]\n' - Ignore this message"
+};
