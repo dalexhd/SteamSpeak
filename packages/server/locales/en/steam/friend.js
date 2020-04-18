1c7145870cfdb863dd459c15371b692aa4d5e05e
DalexHD <eldalexhd@gmail.com>
2020-04-18 16:53:19 +0200

    feat: Add spanish language.

---
+++ packages/server/locales/en/steam/friend.js
@@ -0,0 +1,3 @@
+module.exports = {
+	deleted: 'Your SteamSpeak account has been deleted from our system.'
+};
---
+++ packages/server/locales/en/steam/index.js
@@ -0,0 +1,7 @@
+const status = require('./status');
+const friend = require('./friend');
+
+module.exports = {
+	status,
+	friend
+};
