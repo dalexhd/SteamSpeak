86831e1961ab4a7f63764adf49c747c66e1f9e73
DalexHD <eldalexhd@gmail.com>
2020-04-18 18:27:41 +0200

    fix: Remove file header and client warnings.

--- packages/client/src/views/NotAuthorized.vue
+++ packages/client/src/views/NotAuthorized.vue
@@ -1,12 +1,3 @@
-<!-- =========================================================================================
-    File Name: NotAuthorized.vue
-    Description: Not Authorized Page
-    ----------------------------------------------------------------------------------------
-    Item Name: Vuexy - Vuejs, HTML & Laravel Admin Dashboard Template
-      Author: Pixinvent
-    Author URL: http://www.themeforest.net/user/pixinvent
-========================================================================================== -->
-
 <template>
 	<div class="h-screen flex w-full bg-img">
 		<div
